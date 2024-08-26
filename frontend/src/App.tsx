3039f83e4d07011607efc48a43dd578c9b8ccabd
esgoet <toddesgo@gmail.com>
2024-08-26 13:05:01 +0200

    feat: make getAllCourses request in frontend with axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import {Course} from "./types/types.ts";
+import axios from 'axios';
 
 export default function App() {
+  const [courses, setCourses] = useState<Course[]>([])
+
+  const fetchCourses = () => {
+    axios.get("/api/courses")
+        .then((response) => setCourses(response.data))
+        .catch((error) => console.log(error))
+  }
+
+  useEffect(()=>{
+    fetchCourses();
+  }, [])
 
   return (
     <>
+      <section>
+        <h2>Courses</h2>
+        {courses.map((course)=> (
+            <article>
+              <h3>{course.name}</h3>
+              <p>{course.description}</p>
+            </article>
+        ))}
+      </section>
     </>
   )
 }
