89bf2b368d4305e81b323a8a28a186c49043e253
esgoet <toddesgo@gmail.com>
2024-09-02 10:05:20 +0200

    feat: add basic oauth login in backend

---
+++ backend/src/main/java/com/github/esgoet/backend/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.github.esgoet.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping("/me")
+    public String getUser(@AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes().get("login").toString();
+    }
+}
