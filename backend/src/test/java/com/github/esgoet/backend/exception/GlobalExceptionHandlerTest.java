8df1acd3f1fcf0702f2d1adaa80dba0c6db98c7f
esgoet <toddesgo@gmail.com>
2024-09-03 17:48:47 +0200

    test: add integration test for GlobalExceptionHandler

---
+++ backend/src/test/java/com/github/esgoet/backend/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,75 @@
+package com.github.esgoet.backend.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GlobalExceptionHandlerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void handleCourseNotFoundExceptionTest() throws Exception {
+        mockMvc.perform(get("/api/courses/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().json("""
+                    {
+                      "message": "No course found with id: 999",
+                      "statusCode": 404
+                    }
+                    """))
+                .andExpect(jsonPath("$.timestamp").exists());
+
+    }
+
+    @Test
+    @WithMockUser
+    void handleUserNotFoundExceptionTest_forStudents() throws Exception {
+        mockMvc.perform(get("/api/students/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().json("""
+                    {
+                      "message": "No student found with id: 999",
+                      "statusCode": 404
+                    }
+                    """))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    @WithMockUser
+    void handleUserNotFoundExceptionTest_forInstructors() throws Exception {
+        mockMvc.perform(get("/api/instructors/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().json("""
+                    {
+                      "message": "No instructor found with id: 999",
+                      "statusCode": 404
+                    }
+                    """))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    @WithMockUser
+    void handleExceptionTest() throws Exception {
+        mockMvc.perform(get("/api/no-endpoint"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().json("""
+                    {
+                      "statusCode": 500
+                    }
+                    """))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+}
