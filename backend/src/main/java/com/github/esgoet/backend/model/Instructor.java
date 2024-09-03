fed71f5b52a7e1a1e89f3edc9a7871a06579a2a0
esgoet <toddesgo@gmail.com>
2024-09-03 11:17:42 +0200

    fix: add @Document to Instructor record
    to point to the right collection

--- backend/src/main/java/com/github/esgoet/backend/model/Instructor.java
+++ backend/src/main/java/com/github/esgoet/backend/model/Instructor.java
@@ -1,7 +1,10 @@
 package com.github.esgoet.backend.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@Document("instructors")
 public record Instructor(
         String id,
         String username,
