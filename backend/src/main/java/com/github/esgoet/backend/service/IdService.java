2b544032f9f5953bf6ea12fbbf1fbe65f0958318
esgoet <toddesgo@gmail.com>
2024-09-18 18:26:54 +0200

    feat-#22: make course id generation more performant with timestamp

--- backend/src/main/java/com/github/esgoet/backend/service/IdService.java
+++ backend/src/main/java/com/github/esgoet/backend/service/IdService.java
@@ -4,31 +4,30 @@ import com.github.esgoet.backend.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.time.Instant;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class IdService {
-    // use courseRepo instead
     private final CourseRepository courseRepository;
 
     public String randomId() {
         return UUID.randomUUID().toString();
     }
 
-    public String generateCourseId(String title, LocalDate startTime) {
-        int cycle = 1;
-        String id = title.toLowerCase().replace(" ", "-") + "-" + startTime.toString().substring(2) + "-";
+    public String generateCourseId(String title) {
+        String timestamp = String.valueOf(Instant.now().hashCode()).substring(1);
+        String id = title.toLowerCase().replace(" ", "-") + "-";
         boolean isUnique = false;
         while (!isUnique) {
-            if (!courseRepository.existsById(id + cycle)) {
+            if (!courseRepository.existsById(id + timestamp)) {
                 isUnique = true;
             } else {
-                cycle += 1;
+                timestamp = String.valueOf(Instant.now().hashCode()).substring(1);
             }
         }
-        return id + cycle;
+        return id + timestamp;
 
     }
 }
