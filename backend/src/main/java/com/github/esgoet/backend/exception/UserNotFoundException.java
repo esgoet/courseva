f634b222d9e74c17d78919290fd9fc2c9006240e
esgoet <toddesgo@gmail.com>
2024-09-18 10:32:41 +0200

    fix: add missing space in error message for UserNotFoundException

--- backend/src/main/java/com/github/esgoet/backend/exception/UserNotFoundException.java
+++ backend/src/main/java/com/github/esgoet/backend/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.github.esgoet.backend.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String userType, String id) {
-        super("No " + userType + "found with id: " + id);
+        super("No " + userType + " found with id: " + id);
     }
 }
