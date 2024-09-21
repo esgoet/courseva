0b84a206c6451aa5939045f475d51238552f200a
esgoet <toddesgo@gmail.com>
2024-09-21 12:48:57 +0200

    style: add Icon and header to account page

--- frontend/src/pages/UserAccountPage.tsx
+++ frontend/src/pages/UserAccountPage.tsx
@@ -4,6 +4,7 @@ import ConfirmDialog from "../components/Shared/ConfirmDialog.tsx";
 import {useState} from "react";
 import {Link} from "react-router-dom";
 import {Button, Grid2, Paper} from "@mui/material";
+import {AccountCircle} from "@mui/icons-material";
 
 type UserAccountPageProps = {
     updateUser: (updatedProperty: string, updatedValue: string) => void,
@@ -23,8 +24,14 @@ export default function UserAccountPage({updateUser, updateStudent, updateInstru
             {user &&
                 <Paper sx={{p:'20px', mt: '10px'}} square={false}>
                     <Grid2 container spacing={2}>
+                        <Grid2 size={12} textAlign={"center"}>
+                            <AccountCircle sx={{fontSize: 80}} color={"primary"}/>
+                        </Grid2>
+                        <Grid2 size={12}>
+                            <h2>Account</h2>
+                        </Grid2>
                         <Grid2 size={{xs: 12, sm: 6}}>
-                            <EditableTextDetail
+                        <EditableTextDetail
                                 inputType={"text"}
                                 label={"Username"}
                                 name={"username"}
@@ -33,12 +40,16 @@ export default function UserAccountPage({updateUser, updateStudent, updateInstru
                                 allowedToEdit={true}/>
                         </Grid2>
                         <Grid2 size={{xs: 12, sm: 6}}>
-                            <EditableTextDetail inputType={"email"} label={"Email"} name={"email"} initialValue={user.email}
+                            <EditableTextDetail inputType={"email"} label={"Email"} name={"email"}
+                                                initialValue={user.email}
                                                 updateFunction={updateUser} allowedToEdit={true}/>
                         </Grid2>
                         <Grid2 size={12}>
-                            <Button onClick={() => setConfirmDelete(true)} variant={"outlined"} fullWidth>Delete Account</Button>
-                            <ConfirmDialog toConfirmId={user.id} toConfirmName={"your account"} toConfirmAction={"delete"} modal={confirmDelete} closeModal={() => setConfirmDelete(false)}
+                            <Button onClick={() => setConfirmDelete(true)} variant={"outlined"} color={"secondary"} fullWidth>Delete
+                                Account</Button>
+                            <ConfirmDialog toConfirmId={user.id} toConfirmName={"your account"}
+                                           toConfirmAction={"delete"} modal={confirmDelete}
+                                           closeModal={() => setConfirmDelete(false)}
                                            toConfirmFunction={deleteUser}/>
                         </Grid2>
                     </Grid2>
